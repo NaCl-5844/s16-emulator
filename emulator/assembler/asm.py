16efb3f7fc849fd4134c3ad57d98404bb9f02f7d
NaCl-5844 <114276082+NaCl-5844@users.noreply.github.com>
2023-09-18 22:41:00 +0100

    v0.3.2

--- emulator/assembler/asm.py
+++ emulator/assembler/asm.py
@@ -11,33 +11,63 @@ import re
 # >
 
 
+# BUG List:
+# 000: Cannot determine if asembly instruction not compatible with format file
+
+class InstructionMatchError(KeyError):
+    pass
+
 
 
 
 def get_assembly(assembly_file):
     assembly = {}
     with open(assembly_file, 'r') as f:
-        for k, v in enumerate(f):
-            split_line = re.split(' ', v, 1)
-            if len(v.split()) >= 2:
-                assembly[k] = (split_line[0], re.split(' \n| ;', split_line[1])[0])
-                print(f"file line number: {k}\nRaw line: {v}Split line: {split_line}\nAssembly: {assembly[k]}\n") # [ DEBUG ]
+        for line_number, line in enumerate(f):
+            split_line = re.split(' ', line, 1)
+            if len(line.split()) >= 2:
+                assembly[line_number] = (split_line[0], (re.split('\n| ;', split_line[1])[0]).rstrip())
+                print(f"file line number: {line_number}\nRaw line: {line}Split line: {split_line}\nAssembly: {assembly[line_number]}\n") # [ DEBUG ]
     return assembly #assembly[line_address] -> (op, 'operand string')
 
 
-# will contain a {set} of (instruction,format) tuples to save time lookup/translation speed
-def get_reference_cache(cleaned_assembly, format_file):
+# Generate a {set} of (instruction,format) tuples to save time lookup/translation speed
+# Also assembles operation(op) codes and sub-operation(subOp) codes, e.g:
+# ret:  XXXXXXX-SSS-BBB-AAA -> 0001110-111-BBB-AAA
+def get_reference_cache(assembly, format_file):
     reference_cache = {}
-    operation_set = {v[0] for v in cleaned_assembly.values()}
+    operation_set = {v[0] for v in assembly.values()}
     with open(format_file, 'r') as f:
         for line in f:
             split_line = line.split()
-            if len(split_line) > 1 and split_line[0] in operation_set:
+            if len(split_line) > 1 and split_line[0] in operation_set: ### BUG 000
                 reference_cache[split_line[0]] = split_line[1].replace('-', '')
     return reference_cache #reference_cache[op] -> format
+
+
+def parse(assembly, reference_cache):
+    print(f"Cleaned assembly:\n{assembly}")
+    bytecode = {}
+    for line, instruction in enumerate(assembly.values()):
+        operation = instruction[0]
+        operands = instruction[1]
+        print(line, operation, operands) # [ DEBUG ]
+        instruction_format = reference_cache.get(operation)
+        if instruction_format == None:
+            raise InstructionMatchError(f"Invalid instruction found: [{operation}] Please review assembly code or use a different _[x]16_format_")
+        print(f"findall: {re.findall('N|C|B|A', instruction_format)}")
+
+
+
+
+        # raise InstructionMatchError:
+
+
 
 
 
 cleaned_assembly = get_assembly('test_asm.s16')
 ref_cache = get_reference_cache(cleaned_assembly, '_s16_format_')
 print(ref_cache)
+parse(cleaned_assembly, ref_cache)
+
