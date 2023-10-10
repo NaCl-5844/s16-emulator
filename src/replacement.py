c4b3bae7c84a5fd9f7fd05b572f11b10ec3f9409
NaCl-5844 <114276082+NaCl-5844@users.noreply.github.com>
2023-10-10 14:44:28 +0100

    v0.5.14

--- src/replacement.py
+++ src/replacement.py
@@ -1,23 +1,3 @@
-
-# TODO:
-# > Find a way to offload cache heirarchy into <module>replacement logic
-
-
-# def lru(cache, way_key, new_entry): # Least Recently Used
-#     # BUG: does not return the replaced entry
-#     # -- doesn't work anyway if this function updates by reference >_>
-#
-#     # NOTE: generating collections.deque instead of lists can improve this code a lot
-#     cache[way_key]['tag'].insert(0, cache[way_key]['tag'].pop(-1)) # remove least recent, [-1], and move into most recent, [0]
-#     cache[way_key]['new'].insert(0, cache[way_key]['new'].pop(-1))
-#     cache[way_key]['data'].insert(0, cache[way_key]['data'].pop(-1))
-#     cache[way_key]['dirty'].insert(0, cache[way_key]['dirty'].pop(-1))
-#     cache[way_key]['tag'][0] = new_entry['tag']
-#     cache[way_key]['new'][0] = new_entry['new']
-#     cache[way_key]['data'][0] = new_entry['data']
-#     cache[way_key]['dirty'][0] = new_entry['dirty']
-
-
 def lru(cache, way_key, new_entry): # Least Recently Used
     # BUG: does not return the replaced entry
     # -- doesn't work anyway if this function updates by reference >_>
