376510de9dd05d0014a78d630f00d0f1487d59e5
NaCl-5844 <114276082+NaCl-5844@users.noreply.github.com>
2023-10-26 19:18:34 +0100

    v0.6.2
    Major bug found in s16's assembler:
      - The code would work 50% of the time after the change in formatting(maybe? or something I added)
      - This was due to the use of [ set() ] which would not follow the exact order when extracting a set of entries from a dictionary
      - Thus, in instructions contents would not always be ordered correctly, due to poor implementation
    
    Work has begun on fixing and improving the assembler -- This will be the 3rd revision of the assembler and it's been a great way to see how much I've learned each time I program it :)

--- src/assembler/asm.py
+++ src/assembler/asm.py
@@ -3,6 +3,7 @@ from sys import argv
 import os.path
 import re
 # Long Term Goals:
+# > Assembly files with formats .s16 and .t16 will automatically pick the correct _format_ file
 # > Code and assemble in the command-line/shell then output to hex/bin file
 
 class InstructionMatchError(KeyError):
@@ -22,7 +23,11 @@ def get_assembly(assembly_file):
             if (type(split_line) == int) or ('_' in split_line):
                 raise AssemblyError
             if len(split_line) >= 2:
-                assembly[line_number] = (split_line[0], (re.split('\n| ;', split_line[1])[0]).rstrip())
+                if split_line[1][0] == ';':
+                    # print('----------------------------------------> F')
+                    assembly[line_number] = (split_line[0], None)
+                else:
+                    assembly[line_number] = (split_line[0], (re.split('\n| ;', split_line[1])[0]).rstrip())
                 # print(f"file line number: {line_number}\nRaw line: {line}Split line: {split_line}\nAssembly: {assembly[line_number]}\n") # [ DEBUG ]
     return assembly #assembly[line_address] -> (op, 'operand string')
 
@@ -44,7 +49,7 @@ def get_reference_cache(assembly, format_file):
     if checksum == 16*line_sum: # lines*16 == lines*format_length
         return reference_cache #reference_cache[op] -> format
     else:
-        raise FormatFileError('Inconsistent bit-lengths in format file.') # BUG 001 -- fixed
+        raise FormatFileError('Inconsistent bit-lengths in format file.')
 
 def parse(assembly, reference_cache, output_format):
     print(f"Cleaned assembly:\n{assembly}")
@@ -53,18 +58,23 @@ def parse(assembly, reference_cache, output_format):
         operation, operands = instruction
         print(file_line, operation, operands)                    # [ DEBUG ]
         instruction_format = reference_cache.get(operation)
-        if instruction_format == None: # BUG 000 -- fixed
+        if instruction_format == None:
             raise InstructionMatchError(f"Invalid instruction found: [{operation}] Please review assembly code or use a different _[x]16_format_")
-        bytecode[file_line] = (reference_cache[operation],
-                               operands,
-                               list(set(re.findall('N|C|B|A', instruction_format))))
-        # print(f"Incomplete bytecode: {bytecode[line]}\n")  # [ DEBUG ]
+        bytecode[file_line] = (reference_cache[operation], operands, re.sub('[01XSZ]', '',instruction_format)) # BUG: list(set(re.findall('N|C|B|A', instruction_format)))) DOES NOT PRESERVE ORDER
     for line, partial_bytecode in enumerate(bytecode.values()): # assemble operands
+        print(f"Incomplete bytecode: {bytecode[line]}\n")  # [ DEBUG ]
         code, assembly_operands, format_operands = partial_bytecode
-        for operand_count in range(len(format_operands)): # Nested loop becuase f**k you. That's why.
+        print('----------> ',instruction_format)
+        print('-----------------------> ', set(re.findall('N|C|B|A', instruction_format)))
+        for operand_count in set(re.findall('N|C|B|A', format_operands)): # Nested loop becuase f**k you. That's why.
+            if False:
+                print(line, operand_count)
+                bytecode[line] = code
+                continue
+            print('code:', code,)
             operand_length = len(re.findall(f"{format_operands[-1]}", code))
             operand = format_operands[-1]
-            print(line, operand_count, operand, code, operand_length, assembly_operands, format_operands) # [ DEBUG ]
+            print(line, operand, code, operand_length, assembly_operands, format_operands) # [ DEBUG ]
             target_operand = assembly_operands.rsplit(', ', 1)
             if target_operand[-1][0] == 'r':
                 try:
@@ -84,10 +94,110 @@ def parse(assembly, reference_cache, output_format):
             print(target_value)
             code = (code.replace(operand*operand_length, target_value))
             assembly_operands = target_operand[0]
-            format_operands.pop(-1)
+            format_operands = format_operands.replace(format_operands[-1], '')
             print(code)
             bytecode[line] = code
     return bytecode
+
+
+
+# Generate a {set} of (instruction,format) tuples to save time lookup/translation speed
+# Also assembles operation codes, e.g: ret = XXXXXXX-SSS-BBB-AAA -> 0001110-111-BBB-AAA
+def smart_reference_cache(assembly, format_file):
+    reference_cache = {}
+    operation_set = {v[0] for v in assembly.values()}
+    with open(format_file, 'r') as f:
+        checksum, line_sum = 0, 0 # both variables equal zero
+        for line in f:
+            split_line = line.split()
+            if (len(split_line) > 1) and (split_line[0] in operation_set):
+                clean_line = split_line[1].replace('-', '')
+                operand_set = set(re.findall('N|C|B|A', clean_line))
+                operand_dict = {operand:len(re.findall(operand, clean_line)) for operand in operand_set}
+                reference_cache[split_line[0]] = {'format': clean_line, **operand_dict}
+                print(operand_dict)
+                checksum += len(clean_line) # checksum checks for consistent bit length in source assembly
+                line_sum += 1 # aids in the checksum calculation
+                # print(checksum, line_sum) # [ DEBUG ]
+    if checksum == 16*line_sum: # lines*16 == lines*format_length
+        return reference_cache #reference_cache[op] -> format
+    else:
+        raise FormatFileError('Inconsistent bit-lengths in format file.')
+
+
+def smart_parse(assembly, reference_cache):
+    print('s16_asm:',assembly,'\n\nref_cache:',reference_cache)
+    bytecode = {}
+    for line, instruction in enumerate(assembly.values()):
+        operation = instruction[0]
+        operands = instruction[1]
+        print(line, instruction)
+        # .split(',', 1)
+        config = dict(reference_cache[operation]) # dict() creates a new instance/object? -- if config is a reference, pop() will delete values inside reference_cache
+        partial_bytecode = config.pop('format')
+        cleaned_format = partial_bytecode.strip('01XSZ')
+        print(cleaned_format)
+        if cleaned_format == '':
+            print('----------------------------------------> F')
+            bytecode[line] = partial_bytecode
+            continue
+        print(operands)
+        for operand_count in config:
+            format_operand = cleaned_format[0]
+            bit_length = config[format_operand]
+            # formatted_operand = target_operand * bit_length # e.g. 'A' * 4 = 'AAAA'
+            assembly_operand = operands.split(',', 1)[0]
+            print(assembly_operand)
+            if (assembly_operand[0] == 'r') and (assembly_operand[1:].isdigit()):
+                binary_operand = f"{int(assembly_operand[1:]):0{bit_length}b}"
+            elif assembly_operand.isdigit():
+                binary_operand = f"{int(assembly_operand):0{bit_length}b}"
+            elif (assembly_operand[0] == '-') and assembly_operand[1:].isdigit():
+                binary_operand = f"{((1<<bit_length)-1)+int(assembly_operand[1:])+1:0{bit_length}b}"
+            else:
+                raise AssemblyError(f"Error found on line {line+1}: {assembly[line]}")
+            operands = operands.removeprefix(f"{assembly_operand},").strip()
+            print(format_operand, bit_length, assembly_operand, binary_operand)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def store_to_file(bytecode, file_name):
     with open(os.path.join(os.path.dirname(__file__), os.pardir, 'bytecode', file_name), 'w') as output_file:
@@ -124,14 +234,18 @@ def main():
         else:
             raise KeyError
     except IndexError:
-        print('\nA format, source assembly file, and an output filename required.')
-        print_help() # prints help then exit()
+        cleaned_assembly = get_assembly('test_asm.s16')
+        ref_cache = smart_reference_cache(cleaned_assembly, '_s16_format_') # Hardcoded format file -- should check if it exists?
+        smart_parse(cleaned_assembly, ref_cache)
+        # print('\nA format, source assembly file, and an output filename required.')
+        # print_help() # prints help then exit()
+        exit()
         # print("Incorrect line arguments.Correct method:\npython asm.py -[b/x/bx/xb] [assembly_in] [filename_out]")
     except KeyError:
         print('\n Please use valid options.')
         print_help() # prints help then exit()
     cleaned_assembly = get_assembly(input_file)
-    ref_cache = get_reference_cache(cleaned_assembly, '_s16v2_format_') # Hardcoded format file -- should check if it exists?
+    ref_cache = get_reference_cache(cleaned_assembly, '_s16_format_') # Hardcoded format file -- should check if it exists?
     binary_code = parse(cleaned_assembly, ref_cache, option[-1])
     store_to_file(binary_code, output_file)
     exit()
