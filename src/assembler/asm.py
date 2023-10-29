5fb90f29e1e84aa13701016000fedb06c8ce6907
NaCl-5844 <114276082+NaCl-5844@users.noreply.github.com>
2023-10-29 03:57:44 +0000

    v0.6.4

--- src/assembler/asm.py
+++ src/assembler/asm.py
@@ -31,12 +31,12 @@ def get_assembly(assembly_file):
                 # print(f"file line number: {line_number}\nRaw line: {line}Split line: {split_line}\nAssembly: {assembly[line_number]}\n") # [ DEBUG ]
     return assembly #assembly[line_address] -> (op, 'operand string')
 
-# Generate a {set} of (instruction,format) tuples to save time lookup/translation speed
-# Also assembles operation codes, e.g: ret = XXXXXXX-SSS-BBB-AAA -> 0001110-111-BBB-AAA
-def get_reference_cache(assembly, format_file):
+
+
+def get_reference_cache(assembly, format_file): # Generate a {set} of (instruction,format) tuples to save time lookup/translation speed
     reference_cache = {}
     operation_set = {v[0] for v in assembly.values()}
-    with open(format_file, 'r') as f:
+    with open(format_file, 'r') as f: # file also contains opcodes(X) and subops(S), e.g: XXXXXXX-SSS-BBB-AAA -> 0001110-111-BBB-AAA
         checksum, line_sum = 0, 0 # both variables equal zero
         for line in f:
             split_line = line.split()
@@ -48,35 +48,47 @@ def get_reference_cache(assembly, format_file):
                 print(operand_dict)
                 checksum += len(clean_line) # checksum checks for consistent bit length in source assembly
                 line_sum += 1 # aids in the checksum calculation
-                # print(checksum, line_sum) # [ DEBUG ]
     if checksum == 16*line_sum: # lines*16 == lines*format_length
         return reference_cache #reference_cache[op] -> format
     else:
         raise FormatFileError('Inconsistent bit-lengths in format file.')
 
 
+def get_addressing_modes(format_file):
+    all_instructions = []
+    with open(format_file, 'r') as f:
+        for instruction_format in f:
+            split_instruction_format = instruction_format.split()
+            if (len(split_instruction_format) > 2) and ('_' not in instruction_format):
+                all_instructions.insert(0, split_instruction_format[-1])
+    print(set(all_instructions))
+
+def get_assembly_instructions(format_file):
+    all_instructions = []
+    with open(format_file, 'r') as f:
+        for instruction_format in f:
+            split_instruction_format = instruction_format.split()
+            if (len(split_instruction_format) > 2) and ('_' not in instruction_format):
+                all_instructions.insert(0, split_instruction_format[0])
+    print(set(all_instructions))
+
+
+
 def parse(assembly, reference_cache, implement_formats):
-    print('s16_asm:',assembly,'\n\nref_cache:',reference_cache)
     bytecode = {}
     for line, instruction in enumerate(assembly.values()):
         operation = instruction[0]
         operands = instruction[1]
-        print(line,'\b|',line, instruction)
-        # .split(',', 1)
         config = dict(reference_cache[operation]) # dict() creates a new instance/object? -- if config is a reference, pop() will delete values inside reference_cache
         partial_bytecode = config.pop('format')
         cleaned_format = partial_bytecode.strip('01XSZ')
-        print(line,'\b|',cleaned_format)
         if cleaned_format == '':
             bytecode[line] = partial_bytecode
-            print(line,'\b|',partial_bytecode,'\n')
-            continue
-        print(line,'\b|',operands)
+            continue # No assembling required, move onto next iteration
         for operand_count in config:
             format_operand = cleaned_format[0]
             bit_length = config[format_operand]
             assembly_operand = operands.split(',', 1)[0]
-            print(line,'\b|',assembly_operand)
             if (assembly_operand[0] == 'r') and assembly_operand[1:].isdigit():
                 binary_operand = f"{int(assembly_operand[1:]):0{bit_length}b}"
             elif assembly_operand.isdigit():
@@ -87,10 +99,8 @@ def parse(assembly, reference_cache, implement_formats):
                 raise AssemblyError(f"Error found on line {line+1}: {assembly[line]}")
             operands = operands.removeprefix(f"{assembly_operand},").strip()
             cleaned_format = cleaned_format.strip(format_operand)
-            print(line,'\b|', format_operand, bit_length, assembly_operand, binary_operand)
             partial_bytecode = partial_bytecode.replace(format_operand * bit_length, binary_operand) # e.g. 'A' * 4 = 'AAAA'
         bytecode[line] = partial_bytecode
-        print(line,'\b|',partial_bytecode,'\n')
     return bytecode
 
 
@@ -100,7 +110,8 @@ def store_to_file(bytecode, file_name):
             output_file.writelines(f"{bytecode[line]}\n")
             print(bytecode[line]) # [ DEBUG ]
 
-def print_help():
+def print_help(ExtraComments=""):
+    print(ExtraComments)
     print("""
     OPTION\tDESCRIPTION
 
@@ -112,6 +123,13 @@ def print_help():
     -bh\t\tBoth formats, binary first
     -hb\t\tBoth formats, hex first
 
+
+    EXTRAS\tOPTIONS
+
+    asm.py --get [item]
+
+    --get\taddressingModes, instructions
+
     Place assembly in /assembly
     Generated binaries are placed in s16-emulator/src/bytecode
     """)
@@ -120,27 +138,38 @@ def print_help():
 def main():
     if '-h' in argv or '--help' in argv:
         print_help() # prints help then exit()
-    try:
-        option = argv[-3]
-        if option in ['-h', '-b', '-x', '-bx', '-bx']:
-            print(option)
-            input_file = argv[-2]
-            output_file = argv[-1]
-        else:
-            raise KeyError
-    except IndexError:
-        print('\nA format, source assembly file, and an output filename required.')
-        print_help() # prints help then exit()
+    elif '--get' in argv:
+        try:
+            if argv[argv.index('--get')+1] == 'addressingModes':
+               get_addressing_modes('_s16_format_')
+            if argv[argv.index('--get')+1] == 'instructions':
+               get_assembly_instructions('_s16_format_')
+        except LineArgumentError:
+            print_help()
+        except IndexError:
+            print_help('\n    ERROR: --get must be followed by an item!')
+    else:
+        try:
+            option = argv[-3]
+            if option in ['-h', '-b', '-x', '-bx', '-bx']:
+                print(option)
+                input_file = argv[-2]
+                output_file = argv[-1]
+            else:
+                raise KeyError
+        except IndexError:
+            print('\nA format, source assembly file, and an output filename required.')
+            print_help() # prints help then exit()
 
-        # print("Incorrect line arguments.Correct method:\npython asm.py -[b/x/bx/xb] [assembly_in] [filename_out]")
-    except KeyError:
-        print('\n Please use valid options.')
-        print_help() # prints help then exit()
-    cleaned_assembly = get_assembly(input_file)
-    ref_cache = get_reference_cache(cleaned_assembly, '_s16_format_') # Hardcoded format file -- should check if it exists?
-    binary_code = parse(cleaned_assembly, ref_cache, option[-1])
-    store_to_file(binary_code, output_file)
-    exit()
+            # print("Incorrect line arguments.Correct method:\npython asm.py -[b/x/bx/xb] [assembly_in] [filename_out]")
+        except KeyError:
+            print('\n Please use valid options.')
+            print_help() # prints help then exit()
+        cleaned_assembly = get_assembly(input_file)
+        ref_cache = get_reference_cache(cleaned_assembly, '_s16_format_') # Hardcoded format file -- should check if it exists?
+        binary_code = parse(cleaned_assembly, ref_cache, option[-1])
+        store_to_file(binary_code, output_file)
+        exit()
 
 if __name__ == "__main__":
     main()
